a4a771beb20dcc02a657425d112f0f7a85b548f2
eryz <eryzero@gmail.com>
2019-07-06 13:15:21 +0700

    Modify fourth.py

--- fourth.py
+++ fourth.py
@@ -1,20 +1,20 @@
 def buyNoodle(date, money):
     price = 1650
     amount = money // price
-    if date % 5 == 0:
+    if date % 5:
+        if date % 2:
+            bonus = amount // 3
+            amount = amount + bonus
+        else:
+            bonus = amount // 4
+            amount = amount + bonus
+    else:
         if date % 2:
             bonus = amount // 3
             amount = amount + (bonus*5)
         else:
             bonus = amount // 4
             amount = amount + (bonus*10)
-    else:
-        if date % 2:
-            bonus = amount // 3
-            amount = amount + bonus
-        else:
-            bonus = amount // 4
-            amount = amount + bonus
     return amount
 
 
